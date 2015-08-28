4343b805fde303cf8832c1b21cf53f9527718515
Peter Martini <PeterCMartini@GMail.com>
2015-08-27 20:56:57 -0400

    Take control of how libadd.so is found
    The alternatives are:
    1. Updating LD_LIBRARY_PATH before starting
    2. Making sure libadd.so is in a well-known location
    3. Encoding the *installed* path into the ELF
    
    None of these are very nice.

--- gotest.pm
+++ gotest.pm
@@ -1,6 +1,21 @@
 package gotest;
 our $VERSION = '0.01';
 
+# Preload the libadd so file
+# We need to do this manually, since otherwise it'll
+# follow ld's default behavior and look in either LD_LIBRARY_PATH
+# (which won't have our arch/auto/gotest directory)
+# or in paths hard-coded into the library
+# (which is even less obvious than doing a relative search from this PM)
+{
+  use File::Basename;
+  use DynaLoader;
+  my (undef, $dir) = fileparse(__FILE__);
+  my $libpath = "$dir/../arch/auto/" . __PACKAGE__ . "/libadd.so";
+  # The '1' is necessary to make the symbols visible to the upcoming load
+  DynaLoader::dl_load_file($libpath, 1);
+}
+
 use XSLoader;
 XSLoader::load('gotest', $VERSION);
 
